51bb0218ec48f88d76c60c9bbd533d65becf86f3
mati <maticolis1999_sacchi@hotmail.com>
2022-12-12 17:36:43 -0300

    Agregado de contenido incial de spring

---
+++ Backend/blackuva/src/main/java/com/mati/blackuva/BlackuvaApplication.java
@@ -0,0 +1,13 @@
+package com.mati.blackuva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackuvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackuvaApplication.class, args);
+	}
+
+}
---
+++ Backend/blackuva/src/test/java/com/mati/blackuva/BlackuvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mati.blackuva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackuvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
