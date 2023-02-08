950a390a75bf35b9f168d49ca5bf055289e9bd5f
mati <maticolis1999_sacchi@hotmail.com>
2023-02-08 13:19:19 -0300

    Agregado de auth a front

--- Fontend/vinoteca/src/app/components/users/login/login.component.ts
+++ Fontend/vinoteca/src/app/components/users/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../users/auth.service';
+import { Iuser } from '../users/iuser';
 
 @Component({
   selector: 'app-login',
@@ -8,10 +11,41 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   titulo: String = "Iniciar Sesión"
+  iuser: Iuser ;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router ) {
+    this.iuser = new Iuser();
+   }
 
   ngOnInit(): void {
   }
+
+  login():void{
+    console.log(this.iuser);
+    if(this.iuser.username == null || this.iuser.password == null){
+      sweetAlert('Error inicio Sesión', 'Usuario o Contraseña vacios', 'error');
+    }
+    this.authService.login(this.iuser).subscribe(response => {
+      console.log(response);
+
+      let payload = JSON.parse(atob(response.access_token.split(".")[1]));
+      console.log(payload);
+      
+      this.authService.guardarIusuario(response.access_token);
+      this.authService.guardarToken(response.access_token)
+
+      let usuario = this.authService.iusuario;
+
+
+      this.router.navigate(['/usuarios']);
+      sweetAlert('Inicio de Seccion', `Hola ${usuario.username}, has iniciado sesíon correctamente!` , 'success')
+      
+    });
+
+
+  }
+  
+
+
 
 }
---
+++ Fontend/vinoteca/src/app/components/users/users/iuser.ts
@@ -0,0 +1,9 @@
+export class Iuser {
+    id!: number;
+    username!: string;
+    password!: string;
+    name!: string;
+    lastname!: string;
+    email!: string;
+    roles: string[] = [];
+}
